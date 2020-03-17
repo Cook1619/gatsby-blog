28536ccf8db81edb69879918fab755e3db316467
Cook1619 <cook6348@gmail.com>
2020-03-16 20:22:41 -0500

    Gatsby starter blog

---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const BlogPost = () => (
+    <div>
+        <h1>My Blog</h1>
+        <p>Posts will show up here</p>
+    </div>
+)
+
+export default BlogPost;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hello!</h1>
+            <p>I'm Matt, living in MN</p>
+        </div>
+    )
+}
+
+export default IndexPage;
